d6aa2b73e4f1dbf4b1bffc4d5996199448c8720f
RobN <RobN@ROBN>
2022-06-23 10:28:28 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Add(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Eklendi: " + musteri.Adi);
+        }
+
+        public void Remove(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Silindi!" + musteri.Soyadi);
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Adi = "Mem Robin";
+            musteri1.Soyadi = "Çen";
+            musteri1.Id = 202201;
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Adi = "Ahmet";
+            musteri2.Soyadi = "Yılmaz";
+            musteri2.Id = 202202;
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Add(musteri1);
+            musteriManager.Add(musteri2);
+            musteriManager.Remove(musteri2);
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2 };
+
+            foreach (Musteri musteri in musteriler)
+            {
+                Console.WriteLine("Müşteri No: " + musteri.Id);
+                Console.WriteLine("Müştedi Adı: " + musteri.Adi);
+                Console.WriteLine("Müşteri Soyadı: " + musteri.Soyadi);
+            }
+        }
+    }
+}
